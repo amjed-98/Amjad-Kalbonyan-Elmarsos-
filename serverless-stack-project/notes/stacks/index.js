99934581c463d864a6440b0499a104feeed0b767
Amjad Yahia Roben Hassan <amjedyehia1998@gmail.com>
2022-01-24 21:08:53 +0200

    Securing the API

--- serverless-stack-project/notes/stacks/index.js
+++ serverless-stack-project/notes/stacks/index.js
@@ -1,11 +1,16 @@
 import StorageStack from './StorageStack';
+import AuthStack from './AuthStack';
 import ApiStack from './ApiStack';
 
 export default function main(app) {
   const storageStack = new StorageStack(app, 'storage');
 
-  // eslint-disable-next-line no-new
-  new ApiStack(app, 'api', {
+  const apiStack = new ApiStack(app, 'api', {
     table: storageStack.table,
+  });
+
+  new AuthStack(app, 'auth', {
+    api: apiStack.api,
+    bucket: storageStack.bucket,
   });
 }
